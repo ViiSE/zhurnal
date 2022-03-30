27ffb409f5332ceb075bb3d667d92e9f64529655
ViiSE <viise@outlook.com>
2022-03-31 03:09:48 +1000

    Added TmlDebug javadoc

--- src/main/java/org/viise/zhurnal/tml/TmlDebug.java
+++ src/main/java/org/viise/zhurnal/tml/TmlDebug.java
@@ -25,13 +25,29 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Debug leveled template.
+ * Debug template.
+ * Level of template is {@link Level#DEBUG}.
  */
 public final class TmlDebug implements TemplateLeveled {
 
+    /**
+     * Classname of template.
+     */
     private final Class<?> lgClass;
+
+    /**
+     * Use fully name of {@link #lgClass} or not.
+     */
     private final Boolean useFullName;
+
+    /**
+     * Timestamp of template.
+     */
     private final Template tmlTimestamp;
+
+    /**
+     * The list of templates. These templates will print in the entry template in order in which they were specified.
+     */
     private final List<Template> tmls;
 
     /**
@@ -48,146 +64,334 @@ public final class TmlDebug implements TemplateLeveled {
         this.tmls = tmls;
     }
 
-    /* Ctor 2 */
+    /**
+     * Ctor 2.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlDebug(Class<?> lgClass, Boolean useFullName, Template tmlTimestamp, Template... tmls) {
         this(lgClass, useFullName, tmlTimestamp, Arrays.asList(tmls));
     }
 
-    /* Ctor 3 */
+    /**
+     * Ctor 3.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Boolean useFullName, Template tmlTimestamp, String msg) {
         this(lgClass, useFullName, tmlTimestamp, new TmlMsg(msg));
     }
 
-    /* Ctor 4 */
+    /**
+     * Ctor 4. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Boolean useFullName, String msg) {
         this(lgClass, useFullName, new TmlTimestamp(), new TmlMsg(msg));
     }
 
-    /* Ctor 5 */
+    /**
+     * Ctor 5. {@link #tmlTimestamp} is now. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Boolean useFullName, String msg) {
         this(TmlDebug.class, useFullName, new TmlTimestamp(), new TmlMsg(msg));
     }
 
-    /* Ctor 6 */
+    /**
+     * Ctor 6. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Boolean useFullName, String msg, List<Object> params) {
         this(lgClass, useFullName, new TmlTimestamp(), new TmlMsg(msg, params));
     }
 
-    /* Ctor 7 */
+    /**
+     * Ctor 7. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Boolean useFullName, String msg, Object... params) {
         this(lgClass, useFullName, new TmlTimestamp(), new TmlMsg(msg, params));
     }
 
-    /* Ctor 8 */
+    /**
+     * Ctor 8. {@link #tmlTimestamp} is now. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Boolean useFullName, String msg, List<Object> params) {
         this(TmlDebug.class, useFullName, new TmlTimestamp(), new TmlMsg(msg, params));
     }
 
-    /* Ctor 9 */
+    /**
+     * Ctor 9. {@link #tmlTimestamp} is now. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Boolean useFullName, String msg, Object... params) {
         this(TmlDebug.class, useFullName, new TmlTimestamp(), new TmlMsg(msg, params));
     }
 
-    /* Ctor 10 */
+    /**
+     * Ctor 10. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The list of templates.
+     */
     public TmlDebug(Class<?> lgClass, Template tmlTimestamp, List<Template> tmls) {
         this(lgClass, true, tmlTimestamp, tmls);
     }
 
-    /* Ctor 11 */
+    /**
+     * Ctor 11. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param lgClass Classname of template.
+     * @param tmls The list of templates.
+     */
     public TmlDebug(Class<?> lgClass, List<Template> tmls) {
         this(lgClass, true, new TmlTimestamp(), tmls);
     }
 
-    /* Ctor 12 */
+    /**
+     * Ctor 12. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param tmls The list of templates.
+     */
     public TmlDebug(List<Template> tmls) {
         this(TmlDebug.class, true, new TmlTimestamp(), tmls);
     }
 
-    /* Ctor 13 */
+    /**
+     * Ctor 13. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The list of templates.
+     */
     public TmlDebug(Template tmlTimestamp, List<Template> tmls) {
         this(TmlDebug.class, true, tmlTimestamp, tmls);
     }
 
-    /* Ctor 14 */
+    /**
+     * Ctor 14. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlDebug(Class<?> lgClass, Template tmlTimestamp, Template... tmls) {
         this(lgClass, tmlTimestamp, Arrays.asList(tmls));
     }
 
-    /* Ctor 15 */
+    /**
+     * Ctor 15. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param tmls The array of templates.
+     */
     public TmlDebug(Class<?> lgClass, Template... tmls) {
         this(lgClass, new TmlTimestamp(), Arrays.asList(tmls));
     }
 
-    /* Ctor 16 */
+    /**
+     * Ctor 16. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlDebug(Template tmlTimestamp, Template... tmls) {
         this(TmlDebug.class, tmlTimestamp, Arrays.asList(tmls));
     }
 
-    /* Ctor 17 */
+    /**
+     * Ctor 17. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param tmls The array of templates.
+     */
     public TmlDebug(Template... tmls) {
         this(TmlDebug.class, new TmlTimestamp(), Arrays.asList(tmls));
     }
 
-    /* Ctor 18 */
+    /**
+     * Ctor 18. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Template tmlTimestamp, String msg) {
         this(lgClass, tmlTimestamp, new Template[] { new TmlMsg(msg, new ArrayList<>()) });
     }
 
-    /* Ctor 19 */
+    /**
+     * Ctor 19. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, String msg) {
         this(lgClass, new TmlTimestamp(), new Template[] { new TmlMsg(msg, new ArrayList<>()) });
     }
 
-    /* Ctor 20 */
+    /**
+     * Ctor 20. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Template tmlTimestamp, String msg) {
         this(TmlDebug.class, tmlTimestamp, msg);
     }
 
-    /* Ctor 21 */
+    /**
+     * Ctor 21. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(String msg) {
         this(TmlDebug.class, new TmlTimestamp(), msg);
     }
 
-    /* Ctor 22 */
+    /**
+     * Ctor 22. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Template tmlTimestamp, String msg, List<Object> params) {
         this(lgClass, tmlTimestamp, new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 23 */
+    /**
+     * Ctor 23. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, String msg, List<Object> params) {
         this(lgClass, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 24 */
+    /**
+     * Ctor 24. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(String msg, List<Object> params) {
         this(TmlDebug.class, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 25 */
+    /**
+     * Ctor 25. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, Template tmlTimestamp, String msg, Object... params) {
         this(lgClass, tmlTimestamp, new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 26 */
+    /**
+     * Ctor 26. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param lgClass Classname of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Class<?> lgClass, String msg, Object... params) {
         this(lgClass, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 27 */
+    /**
+     * Ctor 27. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlInfo.class}.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(Template tmlTimestamp, String msg, Object... params) {
         this(TmlDebug.class, tmlTimestamp, new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 28 */
+    /**
+     * Ctor 28. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlInfo.class}.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlDebug(String msg, Object... params) {
         this(TmlDebug.class, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
+    /**
+     * Creating debug template.
+     * Examples:
+     * <pre> {@code
+     * String tml1 = new TmlDebug("Hello, {}!", "log").create();
+     * // This code creating template:
+     * // [LEVEL DEBUG] [TIMESTAMP 2022-03-31T02:09:59.441] [CLASS org.viise.zhurnal.tml.TmlEntry] [MESSAGE Hello, log!]
+     *
+     * String tml2 = new TmlDebug(new Template[] { new TmlMsg("Hello, log!") }).create();
+     * String tml3 = new TmlDebug("Hello, log!");
+     * } </pre>
+     *
+     * @return template as a String.
+     */
     @Override
     public String create() {
         return new TmlEntry(lgClass, useFullName, Level.DEBUG, tmlTimestamp, tmls).create();
     }
 
+    /**
+     * Level of template.
+     *
+     * @return {@link Level#DEBUG}.
+     */
     @Override
     public Level level() {
         return Level.DEBUG;
