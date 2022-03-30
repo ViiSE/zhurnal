b3eefe15321a6ed7974f3bb600a29fdc276ead38
ViiSE <viise@outlook.com>
2022-03-31 02:16:57 +1000

    Added TmlEntry javadoc

--- src/main/java/org/viise/zhurnal/tml/TmlEntry.java
+++ src/main/java/org/viise/zhurnal/tml/TmlEntry.java
@@ -24,15 +24,45 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Entry template.
+ * Entry template is a basic leveled template implementation.
+ */
 public final class TmlEntry implements TemplateLeveled {
 
+    /**
+     * Classname of template.
+     */
     private final Class<?> lgClass;
+
+    /**
+     * Use fully name of {@link #lgClass} or not.
+     */
     private final Boolean useFullName;
+
+    /**
+     * {@link Level} of template.
+     */
     private final Level lvl;
+
+    /**
+     * Timestamp of template.
+     */
     private final Template tmlTimestamp;
+
+    /**
+     * The list of templates. These templates will print in the entry template in order in which they were specified.
+     */
     private final List<Template> tmls;
 
-    /* Ctor 1*/
+    /**
+     * Ctor 1.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The list of templates.
+     */
     public TmlEntry(Class<?> lgClass, Boolean useFullName, Level lvl, Template tmlTimestamp, List<Template> tmls) {
         this.lgClass = lgClass;
         this.useFullName = useFullName;
@@ -41,111 +71,269 @@ public final class TmlEntry implements TemplateLeveled {
         this.tmls = tmls;
     }
 
-    /* Ctor 2 */
+    /**
+     * Ctor 2.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Class<?> lgClass, Boolean useFullName, Level lvl, Template tmlTimestamp, Template... tmls) {
         this(lgClass, useFullName, lvl, tmlTimestamp, Arrays.asList(tmls));
     }
 
-    /* Ctor 3 */
+    /**
+     * Ctor 3.
+     * @param lgClass Classname of template.
+     * @param useFullName Use fully name of {@link #lgClass} or not.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Boolean useFullName, Level lvl, Template tmlTimestamp, String msg) {
         this(lgClass, useFullName, lvl, tmlTimestamp, new TmlMsg(msg));
     }
 
-    /* Ctor 4 */
+    /**
+     * Ctor 4. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, Template tmlTimestamp, List<Template> tmls) {
         this(lgClass, true, lvl, tmlTimestamp, tmls);
     }
 
-    /* Ctor 5 */
+    /**
+     * Ctro 5. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, List<Template> tmls) {
         this(lgClass, true, lvl, new TmlTimestamp(), tmls);
     }
 
-    /* Ctor 6 */
+    /**
+     * Ctor 6. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Level lvl, List<Template> tmls) {
         this(TmlEntry.class, true, lvl, new TmlTimestamp(), tmls);
     }
 
-    /* Ctor 7 */
+    /**
+     * Ctor 7. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Level lvl, Template tmlTimestamp, List<Template> tmls) {
         this(TmlEntry.class, true, lvl, tmlTimestamp, tmls);
     }
 
-    /* Ctor 8 */
+    /**
+     * Ctor 8. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, Template tmlTimestamp, Template... tmls) {
         this(lgClass, lvl, tmlTimestamp, Arrays.asList(tmls));
     }
 
-    /* Ctor 9 */
+    /**
+     * Ctor 9. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, Template... tmls) {
         this(lgClass, lvl, new TmlTimestamp(), Arrays.asList(tmls));
     }
 
-    /* Ctor 10 */
+    /**
+     * Ctor 10. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Level lvl, Template tmlTimestamp, Template... tmls) {
         this(TmlEntry.class, lvl, tmlTimestamp, Arrays.asList(tmls));
     }
 
-    /* Ctor 11 */
+    /**
+     * Ctor 11. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param tmls The array of templates.
+     */
     public TmlEntry(Level lvl, Template... tmls) {
         this(TmlEntry.class, lvl, new TmlTimestamp(), Arrays.asList(tmls));
     }
 
-    /* Ctor 12 */
+    /**
+     * Ctor 12. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, Template tmlTimestamp, String msg) {
         this(lgClass, lvl, tmlTimestamp, new Template[] { new TmlMsg(msg, new ArrayList<>()) });
     }
 
-    /* Ctor 13 */
+    /**
+     * Ctor 13. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, String msg) {
         this(lgClass, lvl, new TmlTimestamp(), new Template[] { new TmlMsg(msg, new ArrayList<>()) });
     }
 
-    /* Ctor 14 */
+    /**
+     * Ctor 14. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Level lvl, Template tmlTimestamp, String msg) {
         this(TmlEntry.class, lvl, tmlTimestamp, msg);
     }
 
-    /* Ctor 15 */
+    /**
+     * Ctor 15. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Level lvl, String msg) {
         this(TmlEntry.class, lvl, new TmlTimestamp(), msg);
     }
 
-    /* Ctor 16 */
+    /**
+     * Ctor 16. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, Template tmlTimestamp, String msg, List<Object> params) {
         this(lgClass, lvl, tmlTimestamp, new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 17 */
+    /**
+     * Ctor 17. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, String msg, List<Object> params) {
         this(lgClass, lvl, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 18 */
+    /**
+     * Ctor 18. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Level lvl, String msg, List<Object> params) {
         this(TmlEntry.class, lvl, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 19 */
+    /**
+     * Ctor 19. {@link #useFullName} is {@code true}.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, Template tmlTimestamp, String msg, Object... params) {
         this(lgClass, lvl, tmlTimestamp, new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 20 */
+    /**
+     * Ctor 20. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now.
+     * @param lgClass Classname of template.
+     * @param lvl {@link Level} of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Class<?> lgClass, Level lvl, String msg, Object... params) {
         this(lgClass, lvl, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 21 */
+    /**
+     * Ctor 21. {@link #useFullName} is {@code true}. {@link #lgClass} is {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param tmlTimestamp Timestamp of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Level lvl, Template tmlTimestamp, String msg, Object... params) {
         this(TmlEntry.class, lvl, tmlTimestamp, new Template[] { new TmlMsg(msg, params) });
     }
 
-    /* Ctor 22 */
+    /**
+     * Ctor 22. {@link #useFullName} is {@code true}. {@link #tmlTimestamp} is now. {@link #lgClass} is
+     * {@code TmlEntry.class}.
+     * @param lvl {@link Level} of template.
+     * @param msg Message. Supports parameter substitution via {@code {}}.
+     * @param params Params of message.
+     *
+     * @see TmlMsg
+     */
     public TmlEntry(Level lvl, String msg, Object... params) {
         this(TmlEntry.class, lvl, new TmlTimestamp(), new Template[] { new TmlMsg(msg, params) });
     }
 
+    /**
+     * Creating entry template.
+     * Examples:
+     * <pre> {@code
+     * String tml1 = new TmlEntry(Level.INFO, "Hello, {}!", "log").create();
+     * // This code creating template:
+     * // [LEVEL INFO] [TIMESTAMP 2022-03-31T02:09:59.441] [CLASS org.viise.zhurnal.tml.TmlEntry] [MESSAGE Hello, log!]
+     *
+     * String tml2 = new TmlEntry(Level.INFO, new Template[] { new TmlMsg("Hello, log!") }).create();
+     * } </pre>
+     *
+     * @return template as a String.
+     */
     @Override
     public String create() {
         StringBuilder sb = new StringBuilder(new TmlLevel(lvl).create())
@@ -157,6 +345,11 @@ public final class TmlEntry implements TemplateLeveled {
         return sb.toString();
     }
 
+    /**
+     * Level of template.
+     *
+     * @return {@link #lvl}.
+     */
     @Override
     public Level level() {
         return lvl;
